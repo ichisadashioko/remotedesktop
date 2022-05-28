6a68f41c99bd013e3f05e8f6de0af6dca71683f9
shioko <ichisadashioko@gmail.com>
2022-05-28 19:08:57 +0000

    :art: refactor webserver

--- remotedesktopwebserver.py
+++ remotedesktopwebserver.py
@@ -26,13 +26,27 @@ import cv2
 
 import mss
 
+DEFAULT_SERVER_PORT = 21578
+
+DEFAULT_FRAME_RATE = 30
+parser = argparse.ArgumentParser(description='Remote desktop webserver')
+parser.add_argument('port', type=int, default=DEFAULT_SERVER_PORT, nargs='?')
+parser.add_argument('--framerate', type=int, default=DEFAULT_FRAME_RATE)
+parser.add_argument('--imageformat', choices=['png', 'jpg'], default='png')
+args = parser.parse_args()
+print('args', args)
+
+PORT_NUMBER = args.port
+FRAME_RATE = args.framerate
+IMAGE_FORMAT = args.imageformat
+IMAGE_EXTENSION = f'.{IMAGE_FORMAT}'
 
 with mss.mss() as sct:
     default_monitor = sct.monitors[1]
     print('default_monitor', default_monitor)
 
 
-def current_screen_in_jpg_string(reduce_size_by_factor: int = None):
+def capture_screen_to_image_bytes(reduce_size_by_factor: int = None):
     with mss.mss() as sct:
         mss_image = sct.grab(default_monitor)
         np_image = np.array(mss_image, dtype=np.uint8)
@@ -52,15 +66,16 @@ def current_screen_in_jpg_string(reduce_size_by_factor: int = None):
 
         print(time.perf_counter_ns(), 'rgb_image.shape', rgb_image.shape, 'tmp', tmp, end='\r')
 
-        status, bs = cv2.imencode('.jpg', rgb_image)
+        status, bs = cv2.imencode(IMAGE_EXTENSION, rgb_image)
         return bs.tobytes()
 
 
-FRAME_RATE = 30
 WAIT_TIME_SECONDS = 1 / FRAME_RATE
 WAIT_TIME_NS = WAIT_TIME_SECONDS * 10 ** 9
 LAST_FRAME_TIME_NS = 0
 CURRENT_FRAME_TIME_NS = 0
+
+IMAGE_CONTENT_TYPE_HEADER = f'Content-Type: image/{IMAGE_FORMAT}'
 
 
 class MainHandler(tornado.web.RequestHandler):
@@ -79,9 +94,9 @@ class MainHandler(tornado.web.RequestHandler):
                 break
 
             self.write('--frame\r\n')
-            self.write('Content-Type: image/jpeg\r\n')
+            self.write(IMAGE_CONTENT_TYPE_HEADER)
 
-            bs = current_screen_in_jpg_string(reduce_size_by_factor)
+            bs = capture_screen_to_image_bytes(reduce_size_by_factor)
             bs_len = len(bs)
             self.write(f'Content-Length: {bs_len}\r\n\r\n')
             self.write(bs)
@@ -92,10 +107,10 @@ class MainHandler(tornado.web.RequestHandler):
 app = tornado.web.Application([
     (r'/', MainHandler),
     (r'/([0-9]+)', MainHandler),
+    (r'/static/(.*)', tornado.web.StaticFileHandler, {'path': 'static'}),
 ])
 
 
-SERVER_PORT = 21578
-print(f'http://localhost:{SERVER_PORT}')
-app.listen(SERVER_PORT, address='0.0.0.0')
+print(f'http://localhost:{PORT_NUMBER}')
+app.listen(PORT_NUMBER, address='0.0.0.0')
 tornado.ioloop.IOLoop.current().start()
